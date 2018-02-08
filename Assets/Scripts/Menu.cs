a7fff2bea5bc19f4a9a329a79fd0e8a03a6f919c
Adams880 <psnadams880@gmail.com>
2018-02-08 17:11:22 -0500

    Initial Commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObj;  
+
+    public GameObject userMenu;
+    public GameObject locomotionMenu;
+
+    public bool userMenuActive = false;
+    public bool locomotionMenuActive = false;
+    //fuck alex
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        userMenu.SetActive(false);
+        locomotionMenu.SetActive(false);
+    }
+
+    void Update ()
+    {
+        if (Controller.GetPressDown(SteamVR_Controller.ButtonMask.ApplicationMenu))
+        {
+            if (!userMenuActive && !locomotionMenuActive)
+            {
+                userMenu.SetActive(true);
+                userMenuActive = true;
+            }
+            else
+            {
+                userMenu.SetActive(false);
+                userMenuActive = false;
+            }
+        }
+    }
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+    public void LocomotionMenuToggle()
+    {
+        if (!locomotionMenuActive)
+        {
+            locomotionMenu.SetActive(true);
+            locomotionMenuActive = true;
+            userMenu.SetActive(false);
+            userMenuActive = false;
+        }
+        else
+        {
+            locomotionMenu.SetActive(false);
+            locomotionMenuActive = false;
+            userMenu.SetActive(true);
+            userMenuActive = true;
+        }
+    }
+}
