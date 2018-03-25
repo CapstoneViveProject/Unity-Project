e61e4012d538480f77f492d70ad27df07b76def7
Wezanard <32334426+Wezanard@users.noreply.github.com>
2018-03-25 09:54:25 -0400

    Timer Update

--- Assets/Scripts/ObstacleCourseSetup.cs
+++ Assets/Scripts/ObstacleCourseSetup.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 public class ObstacleCourseSetup : MonoBehaviour
 {
     public int errors;
-    public float timer;
+    public float timer = 0;
 
+    public bool startLine = true;
+    public bool finishLine = false;
+    private void FixedUpdate()
+    {
+        if(finishLine)
+        {
+
+        }
+        else if(!startLine && !finishLine)
+        {
+            timer += Time.deltaTime;
+        }
+    }
 }
